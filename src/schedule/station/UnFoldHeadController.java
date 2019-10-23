63fa01acc0084af081390165f63d68504d49c9ab
yd_private <690462878@qq.com>
2019-10-23 16:26:09 +0800

    骨架整理

---
+++ src/schedule/station/UnFoldHeadController.java
@@ -0,0 +1,16 @@
+package schedule.station;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class UnFoldHeadController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
