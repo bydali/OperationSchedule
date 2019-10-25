a0c362423d9f88c5cbc5c993a0160a093c60168f
yd_private <690462878@qq.com>
2019-10-25 17:34:44 +0800

    绘制时间线

--- src/test/TestFunc.java
+++ src/test/TestFunc.java
@@ -1,13 +1,40 @@
 package test;
 
+import javax.swing.Painter;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class TestFunc extends Application {
 
@@ -19,7 +46,6 @@ public class TestFunc extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -28,4 +54,5 @@ public class TestFunc extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
--- src/test/TestPaneController.java
+++ src/test/TestPaneController.java
@@ -3,6 +3,8 @@ package test;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.text.Position;
+
 import com.sun.media.jfxmedia.events.NewFrameEvent;
 
 import javafx.beans.value.ChangeListener;
@@ -10,7 +12,10 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Cursor;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseButton;
@@ -18,7 +23,16 @@ import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
 
+/**
+ * 测试运行图左上单元格长宽为，h=40，w=200
+ * 
+ * @author Administrator
+ *
+ */
 public class TestPaneController implements Initializable {
 
 	@FXML
@@ -30,7 +44,11 @@ public class TestPaneController implements Initializable {
 	@FXML
 	private ScrollPane sp;
 	@FXML
-	private AnchorPane operatePane;
+	private AnchorPane operateAp;
+	@FXML
+	private AnchorPane timeAp;
+	@FXML
+	private AnchorPane headsAp;
 
 	private double oldX;
 	private double oldY;
@@ -41,6 +59,68 @@ public class TestPaneController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		syncSliding();
+		setPaneSize();
+		drawTime();
+		drawHeads();
+		drawOperateMap();
+	}
+
+	private void setPaneSize() {
+
+	}
+
+	private final int leftOffset = 20;
+	private final int hourInterval = 240;
+	private final int decadeInterval = hourInterval / 6;
+
+	private void drawTime() {
+		int topOffsetHour = 18;
+		int topOffsetDecade = 23;
+		int lenOfHourLine = 15;
+		int lenOfDecadeLine = 7;
+
+		for (int i = 6; i <= 24; i++) {
+			double startX = leftOffset + (i - 6) * hourInterval;
+			double endY = topOffsetHour + lenOfHourLine;
+			Line hour = new Line(startX, topOffsetHour, startX, endY);
+			hour.setStroke(Color.RED);
+			timeAp.getChildren().add(hour);
+
+			Label hourL = new Label(String.valueOf(i));
+			hourL.setTextFill(Color.RED);
+			hourL.setPadding(new Insets(3, 0, 0, startX - 5));
+			timeAp.getChildren().add(hourL);
+
+			for (int j = 1; j <= 5; j++) {
+				double start = startX + j * decadeInterval;
+				double end = topOffsetDecade + lenOfDecadeLine;
+				Line decade = new Line(start, topOffsetDecade, start, end);
+				decade.setStroke(Color.RED);
+				timeAp.getChildren().add(decade);
+
+				Label decadeL = new Label(String.valueOf(j));
+				decadeL.setFont(new Font(8));
+				decadeL.setTextFill(Color.RED);
+				decadeL.setPadding(new Insets(10, 0, 0, start - 3));
+				timeAp.getChildren().add(decadeL);
+			}
+		}
+
+		double startX = leftOffset;
+		double startY = topOffsetDecade + lenOfDecadeLine / 2;
+		double endX = startX + hourInterval * (24 - 6);
+		Line h = new Line(startX, startY, endX, startY);
+		h.setStroke(Color.RED);
+		h.setStrokeWidth(0.5);
+		timeAp.getChildren().add(h);
+	}
+
+	private void drawHeads() {
+
+	}
+
+	private void drawOperateMap() {
+
 	}
 
 	private void syncSliding() {
@@ -74,8 +154,8 @@ public class TestPaneController implements Initializable {
 					double relativeX = event.getX() - oldX;
 					double relativeY = event.getY() - oldY;
 
-					double relativeHValue = relativeX / (operatePane.getWidth() - operateSp.getWidth());
-					double relativeVValue = relativeY / (operatePane.getHeight() - operateSp.getHeight());
+					double relativeHValue = relativeX / (operateAp.getWidth() - operateSp.getWidth());
+					double relativeVValue = relativeY / (operateAp.getHeight() - operateSp.getHeight());
 					sp.setHvalue(oldHValue + relativeHValue);
 					operateSp.setVvalue(oldVValue + relativeVValue);
 				}
